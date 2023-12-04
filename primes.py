fac1de17673c8ab0befcafe40e4df3e2f5d34841
Omer Osman <omer.m.o.hussain@gmail.com>
2023-12-04 15:57:18 +0000

    Create solution for primes

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(number):
+    if number > 1:
+        for i in range(2, number):
+            if (number % i) == 0:
+                return False
+    else:
+        return False
+    
+    return True
+
 def primes(number_of_primes):
     list = []
+    # add the first number_of_primes primes to the list
+    if number_of_primes <= 0:
+        raise ValueError("number_of_primes must be greater than 0")
+    i = 2
+    while len(list) < number_of_primes:
+        if isPrime(i):
+            list.append(i)
+        i += 1
     return list
